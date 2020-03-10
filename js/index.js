1c66a1f66fcd9b2ea70f20f5beecd360be12c672
Robert Murdoch <rmurdoc90@gmail.com>
2020-03-10 15:03:14 -0700

    3:02 mouse over and keydown done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
 // Your code goes here
+// function clickEventHandler(event) {
+//     console.log(event.type + '!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+//     console.log(event.target)
+//   }
+//   document.querySelector('a').addEventListener('click', clickEventHandler)
+//   document.querySelector('nav').addEventListener('click', clickEventHandler)
+//   document.querySelector('header').addEventListener('click', clickEventHandler)
+//   document.body.addEventListener('click', clickEventHandler)
+//   document.addEventListener('click', clickEventHandler)
+//   window.addEventListener('click', clickEventHandler)
+
+// <----------------Mouse Over------------------------->
+
+let mouseHover = document.querySelector('a');
+mouseHover.addEventListener('mouseover', (event) =>{
+  event.target.style.color = 'red';
+
+  setTimeout(function(){
+    event.target.style.color='black'
+  },500)
+})
+
+// <----------------Mouse Over------------------------->
+
+// <----------------Keydown------------------------->
+
+document.addEventListener('keydown', logKey);
+
+function logKey(e){
+  console.log(e.key)
+}
+
+// <----------------Keydown------------------------->
